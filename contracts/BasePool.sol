fff97c0b738e6519a7a4c2ef3d55099f430d9a32
Matheus Pagani <matheusdaros77@hotmail.com>
2020-04-26 15:55:53 -0300

    Project Setup

---
+++ contracts/BasePool.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.6.4;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Roles.sol";
+
+contract BasePool {
+
+}
