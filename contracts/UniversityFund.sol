f23dd7de5540067b7dec61d51f8dba031f3d4b40
Matheus Pagani <matheusdaros77@hotmail.com>
2020-04-28 13:59:24 -0300

    Base Commit 0.6.6

--- contracts/UniversityFund.sol
+++ contracts/UniversityFund.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.6;
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/access/Roles.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 
 contract UniversityFund is Ownable{
     using SafeMath for uint256;
