a999f8d0ea09daacdb8418480e93a55c7540e0dc
Matheus Pagani <matheusdaros77@hotmail.com>
2020-04-29 16:27:43 -0300

    daiToken Address

--- contracts/Classroom.sol
+++ contracts/Classroom.sol
@@ -1,10 +1,14 @@
 pragma solidity 0.6.6;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Sender.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "./UniversityFund.sol";
 import "./Student.sol";
 
@@ -13,10 +17,13 @@ contract Classroom is Ownable {
 
     bytes32 _name;
     address _universityAddress;
+    IERC20 public daiToken;
 
     constructor(bytes32 name, address universityAddress) public {
         _name = name;
         _universityAddress = universityAddress;
+        //Kovan address
+        daiToken = IERC20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
     }
 
 }
--- contracts/UniversityFund.sol
+++ contracts/UniversityFund.sol
@@ -1,12 +1,24 @@
 pragma solidity 0.6.6;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Sender.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "./Classroom.sol";
 import "./Student.sol";
+
+interface CERC20 {
+    function mint(uint256) external returns (uint256);
+    function exchangeRateCurrent() external returns (uint256);
+    function supplyRatePerBlock() external returns (uint256);
+    function redeem(uint) external returns (uint);
+    function redeemUnderlying(uint) external returns (uint);
+}
 
 contract UniversityFund is Ownable, AccessControl {
     using SafeMath for uint256;
@@ -23,9 +35,14 @@ contract UniversityFund is Ownable, AccessControl {
     // Address list of every donor
     address[] _donors;
 
+    CERC20 public cToken;
+    IERC20 public daiToken;
+
     constructor() public {
         _classList = new address[](0);
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        //Kovan address
+        daiToken = IERC20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
     }
 
     event NewClassroom(bytes32 indexed name, address addr);
