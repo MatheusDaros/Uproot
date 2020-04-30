89bbc6779d96e7ef01c8c9183a250a7437b3101e
Matheus Pagani <matheusdaros77@hotmail.com>
2020-04-30 16:18:48 -0300

    Student application on Classroom, Student score and roles

--- contracts/Student.sol
+++ contracts/Student.sol
@@ -10,22 +10,65 @@ import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Sender.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "./Classroom.sol";
-import "./UniversityFund.sol";
+import "./University.sol";
 
-contract Student is Ownable {
+contract Student is Ownable, AccessControl {
     using SafeMath for uint256;
 
+    //READ_SCORE_ROLE can read student Score
+    bytes32 public constant READ_SCORE_ROLE = keccak256("READ_SCORE_ROLE");
+    //MODIFY_SCORE_ROLE can read student Score
+    bytes32 public constant MODIFY_SCORE_ROLE = keccak256("MODIFY_SCORE_ROLE");
+
     bytes32 _name;
-    address _universityAddress;
+    University _university;
     address[] _classroomAddress;
+    int32 _score;
+
     IERC20 public daiToken;
     CERC20 public cToken;
 
     constructor(bytes32 name, address universityAddress) public {
         _name = name;
-        _universityAddress = universityAddress;
+        _score = 0;
+        _university = University(universityAddress);
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        grantRole(READ_SCORE_ROLE, _msgSender());
+        grantRole(READ_SCORE_ROLE, universityAddress);
+        grantRole(MODIFY_SCORE_ROLE, universityAddress);
+        grantRole(DEFAULT_ADMIN_ROLE, universityAddress);
+        renounceRole(DEFAULT_ADMIN_ROLE, _msgSender());
         //Kovan address
         daiToken = IERC20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
+    }
+
+    function score() public view returns(int32) {
+        require(hasRole(READ_SCORE_ROLE, _msgSender()), "Student: caller doesn't have READ_SCORE_ROLE");
+        return _score;
+    }
+
+    function addScore(int32 val) public  {
+        require(hasRole(MODIFY_SCORE_ROLE, _msgSender()), "Student: caller doesn't have MODIFY_SCORE_ROLE");
+        _score += val;
+    }
+
+    function subtractScore(int32 val) public  {
+        require(hasRole(MODIFY_SCORE_ROLE, _msgSender()), "Student: caller doesn't have MODIFY_SCORE_ROLE");
+        _score -= val;
+    }
+
+    function applyToClassroom(address classroomAddress) public onlyOwner {
+        require(_university.isValidClassroom(classroomAddress), "Student: address is not a valid classroom");
+        grantRole(READ_SCORE_ROLE, classroomAddress);
+        Classroom(classroomAddress).studentApply();
+        _classroomAddress.push(classroomAddress);
+    }
+
+    // not a feature but it is something a teacher would sometimes want to do
+    function removeFromMyClassroom() public {
+        for (uint i = 0; i < _classroomAddress.length; i++) {
+            if (_classroomAddress[i] == _msgSender()) _classroomAddress[i] = address(0);
+        }
     }
 
 }
