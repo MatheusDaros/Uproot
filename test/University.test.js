c4a426116d496b7303aeb76db3c0d9b0023709ec
Flavio Gouvea <flavio@4cadia.com>
2020-05-06 22:28:38 -0300

    University.test.js

--- test/University.test.js
+++ test/University.test.js
@@ -1,44 +1,66 @@
 var web3 = require('web3');
 
-const Artifact = artifacts.require("../contracts/University");
-const daiToken = artifacts.require("@openzeppelin/contracts/token/ERC20/IERC20.sol");
-const Assert = require("truffle-assertions");
+require('dotenv').config();
 
-contract("University", accounts => {
+const Contract = artifacts.require("University");
+//const Dai = artifacts.require("ERC20");
 
-  //const name = process.env.UNIVERSITY_ADDRESS;
-  //const cut = process.env.UNIVERSITY_CUT;
-  //const studentGSNDeposit = process.env.UNIVERSITY_GSNDEPOSIT;
-  //const daiAddress = process.env.UNIVERSITY_DAIADDRESS;
-  //const compoundAddress = process.env.UNIVERSITY_COMPOUNDADDRESS;
-  //const relayHubAddress = process.env.UNIVERSITY_RELAYHUBADDRESS;
+contract("University test", accounts => {
 
-  const tokenName = process.env.TOKEN_NAME;
-  const tokenSymbol = process.env.TOKEN_SYMBOL;
-  const tokenDecimals = process.env.TOKEN_DECIMALS;
-  const tokenTotalSupply = process.env.TOKEN_SUPPLY;
+  /*
 
-  let contractInstance;
-  let dai;
-
-  const ownerAddress = accounts[0];
-  const address1 = accounts[1];
-  const address2 = accounts[2];
-  const address3 = accounts[3];
-
-  before(async () => {
-    web3.eth.defaultAccount = ownerAddress;
-    dai = await daiToken.new(
-      tokenName,
-      tokenSymbol,
-      tokenDecimals,
-      tokenTotalSupply
-    );
+  const tokenName = "dai";
+  const tokenSymbol = "DAI";
+  
+  beforeEach(async () => {
+    await Dai.deployed(tokenName, tokenSymbol);
+    await Dai._mint(accounts[1], 10000);
   });
+
+  */
+
+  //TODO library and require
+  function getAddress(file) {
+    let input = require(file);
+    let dKey = Object.keys(input.networks)[0];
+    return input.networks[dKey].address;
+  }
+
+  //University Params
+  const name = web3.utils.asciiToHex(process.env.UNIVERSITY_NAME);
+  const cut = web3.utils.asciiToHex(process.env.UNIVERSITY_CUT);
+  const studentGSNDeposit = web3.utils.asciiToHex(process.env.UNIVERSITY_GSNDEPOSIT);
+  const compoundAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F" //getAddress('../build/contracts/CERC20.json');
+  const relayHubAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; //getAddress('../build/contracts/CERC20.json');
+  const classroomFactoryAddress = getAddress('../build/contracts/ClassroomFactory.json');
+  const studentFactoryAddress = getAddress('../build/contracts/StudentFactory.json');
+  const studentApplicationFactoryAddress = getAddress('../build/contracts/StudentApplicationFactory.json');
+  const daiAddress = getAddress('../build/contracts/ERC20.json');
 
   describe("constructor", () => {
     it('1 - Deploying contract properly...', async () => {
-      Assert(contractInstance.address != '');
+      await Contract.deployed(name, cut, studentGSNDeposit, compoundAddress, relayHubAddress, classroomFactoryAddress, studentFactoryAddress, studentApplicationFactoryAddress, daiAddress);
+      assert(Contract.address != '');
     });
+
+    it('2 - Store Name...', async () => {
+      await Contract.deployed(name, cut, studentGSNDeposit, compoundAddress, relayHubAddress, classroomFactoryAddress, studentFactoryAddress, studentApplicationFactoryAddress, daiAddress);
+      var UniversityName = await Contract.Name();
+      assert(UniversityName === web3.utils.asciiToHex(process.env.UNIVERSITY_NAME));
+    });
+
+    it('3 - Store Admin Role...', async () => {
+      await Contract.deployed(name, cut, studentGSNDeposit, compoundAddress, relayHubAddress, classroomFactoryAddress, studentFactoryAddress, studentApplicationFactoryAddress, daiAddress);
+      var AdminRole = await Contract.DEFAULT_ADMIN_ROLE();
+      assert(AdminRole === accounts[0]);
+    });
+
+    it('4 - Store Admin Role...', async () => {
+      await Contract.deployed(name, cut, studentGSNDeposit, compoundAddress, relayHubAddress, classroomFactoryAddress, studentFactoryAddress, studentApplicationFactoryAddress, daiAddress);
+      var AdminRole = await Contract.DEFAULT_ADMIN_ROLE();
+      assert(AdminRole != accounts[1]);
+    });
+
   });
+
 });
