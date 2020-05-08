c17045f54ea98ad19f06a0ebb97848bec1bb5429
Matheus Pagani <matheusdaros77@hotmail.com>
2020-05-08 08:20:39 -0300

    Buidler config and gitignore fix

--- buidler.config.js
+++ buidler.config.js
@@ -1,1 +1,20 @@
-module.exports = {};
+usePlugin("@nomiclabs/buidler-waffle");
+
+// This is a sample Buidler task. To learn how to create your own go to
+// https://buidler.dev/guides/create-task.html
+task("accounts", "Prints the list of accounts", async() => {
+    const accounts = await ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(await account.getAddress());
+    }
+});
+
+module.exports = {
+    solc: {
+        version: '0.6.6'
+    },
+    paths: {
+        tests: './testsBuidler'
+    }
+};
---
+++ testsBuidler/University.test.js
@@ -0,0 +1,47 @@
+const { solidity, MockProvider, deployContract } = require('ethereum-waffle');
+const { use, expect } = require('chai');
+const { ethers } = require("@nomiclabs/buidler");
+const University = require('../build/contracts/University.json');
+
+use(solidity);
+require('dotenv').config();
+
+//TODO library and require
+function getAddress(file) {
+    let input = require(file);
+    let dKey = Object.keys(input.networks)[0];
+    return input.networks[dKey].address;
+}
+
+//University Params
+const name = ethers.utils.formatBytes32String(process.env.UNIVERSITY_NAME);
+const cut = process.env.UNIVERSITY_CUT;
+const studentGSNDeposit = process.env.UNIVERSITY_GSNDEPOSIT;
+const relayHubAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; //getAddress('../build/contracts/CERC20.json');
+const classroomFactoryAddress = getAddress('../build/contracts/ClassroomFactory.json');
+const studentFactoryAddress = getAddress('../build/contracts/StudentFactory.json');
+const studentApplicationFactoryAddress = getAddress('../build/contracts/StudentApplicationFactory.json');
+const daiAddress = getAddress('../build/contracts/ERC20.json');
+
+
+describe('University smart contract', () => {
+    const provider = new MockProvider();
+    const [ownerAddress] = provider.getWallets();
+
+    async function deployBefore() {
+        const contract = await deployContract(ownerAddress, University, [name, cut, studentGSNDeposit, relayHubAddress, classroomFactoryAddress, studentFactoryAddress, studentApplicationFactoryAddress, daiAddress]);
+        return contract;
+    }
+
+    it("must register name at deploy", async() => {
+        const contract = await deployBefore();
+        expect(await contract.name()).to.equal(name);
+    });
+
+    //    it('Student register success', async() => {
+    //        const sName = "Flavio Neto";
+    //        await contractInstance.studentSelfRegister(web3.utils.utf8ToHex(sName), { from: student1Address });
+    //        const result = await contractInstance.studentIsRegistered(student1Address, { from: ownerAddress });
+    //        assert.equal(result, true, 'wrong');
+    //    });
+});
