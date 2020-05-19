643075c59c113aed0dbea0896dfc84a783c92dc4
Matheus Pagani <matheusdaros77@hotmail.com>
2020-05-19 13:42:14 -0300

    udate migrations address

--- migrations/5_University_migration.js
+++ migrations/5_University_migration.js
@@ -3,13 +3,13 @@ const Migrations = artifacts.require("University");
 var Web3 = require("web3");
 
 // Using Ropsten
-const name = Web3.utils.asciiToHex("DeEd University");
+const name = Web3.utils.asciiToHex("University");
 const cut = 0.2 * 1e6;
 const relayHubAddress = "0xD216153c06E857cD7f72665E0aF1d7D82172F494"; //same on every network
-const classroomFactoryAddress = "0xcEf0cDb8bb2B6ae4a4212c34489770d3e7b406e9";
-const studentFactoryAddress = "0x55f41e137ABb4F3fA368937399499Bc600Feffa2";
+const classroomFactoryAddress = "0x30D507255D025bB59e0edC626D074cA5Bd99391d";
+const studentFactoryAddress = "0x363966265fa8ab6fE016BF10a8c1e7FA364bD878";
 const studentApplicationFactoryAddress =
-    "0xd86d148A491652E1FBBac3A478fb0a31e78fD456";
+    "0x0F1a2829339c0B5CcD9b6b353a8CD700de562850";
 const daiAddress = "0xad6d458402f60fd3bd25163575031acdce07538d";
 const compoundAddress = "0x6ce27497a64fffb5517aa4aee908b1e7eb63b9ff";
 const ensContractAddress = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"; //same on every network
